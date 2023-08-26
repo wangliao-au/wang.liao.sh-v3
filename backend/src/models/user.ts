305a6a4cb894096a2fc2be5065da7febbebc7d97
lia0wang <lia0wang@outlook.com>
2023-08-26 17:24:07 +1000

    setup backend file struct & user model

---
+++ backend/src/models/user.ts
@@ -0,0 +1,13 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface User extends Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new Schema<User>({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+export default mongoose.model<User>('User', userSchema);
