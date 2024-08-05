9397d7d090b467aba6b842bee3f7af518bc621d5
lia0wang <lia0wang@outlook.com>
2024-08-06 04:52:21 +1000

    add metadata

---
+++ frontend/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Wang Liao",
+  description: "Wang LIAO, SDET / QE at Tyro Payments",
+  metadataBase: new URL("https://wang-liao-sh-v3.vercel.app/"),
+  authors: [
+    { name: "LIAO Wang" },
+    { name: "Wang Liao" },
+    { name: "Leon Liao" },
+  ],
+  keywords: ["wang liao"],
+  creator: "Wang Liao",
+  publisher: "Wang Liao",
+};
+
+export default function RootLayout() {
+  // ...
+}
